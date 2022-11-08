4762c30a6cab843f4b3f207452e984ecd3322279
kokoro <cocosyu.11261126@gmail.com>
2022-11-08 22:45:41 +0900

    [Add] 定数を追加
    ・prefectures(都道府県選択select box)
    ・jobOpenings(求人・求職者数リストを表示させるdl要素)

---
+++ src/js/jobListings/globalVariable.ts
@@ -0,0 +1,4 @@
+// 都道府県選択select box
+export const prefectures = document.getElementById('js-prefectures') as HTMLSelectElement;
+// 求人・求職者数リスト
+export const jobOpenings = document.getElementById('js-job-openings') as HTMLElement;
