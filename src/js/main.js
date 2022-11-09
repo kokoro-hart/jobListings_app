23bb71b7336d9ff986381347176355ce6178f1d3
kokoro <cocosyu.11261126@gmail.com>
2022-11-10 00:36:26 +0900

    [Update]prefecturesとjobOpeningsの存在チェックを追加
    domが存在しない場合の実行時エラーを防ぐため

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,13 @@ import { resizeViewport } from './common/resizeViewport'
 import { addPrefectureList } from './jobListings/addPrefecture'
 import { displayJobSeeker } from './jobListings/displayJobSeeker'
 import { prefecturalSelectionEvents } from './jobListings/prefecturalSelectionEvents'
+import { prefectures, jobOpenings } from './jobListings/globalVariable'
 // scssを読み込み
 import '../scss/style.scss';
+
 resizeViewport();
-addPrefectureList();
-displayJobSeeker();
-prefecturalSelectionEvents();
+if (prefectures && jobOpenings) {
+  addPrefectureList();
+  displayJobSeeker();
+  prefecturalSelectionEvents();
+}
