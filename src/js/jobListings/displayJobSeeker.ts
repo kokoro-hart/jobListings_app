ed71f3c4e5b7d769597e9080c2facfd421c98d37
kokoro <cocosyu.11261126@gmail.com>
2022-11-23 17:43:46 +0900

    [Update] ResasAPIの取得データへの型付け
    加えてjobOpeningsのnullチェックも追加

--- src/js/jobListings/displayJobSeeker.ts
+++ src/js/jobListings/displayJobSeeker.ts
@@ -1,23 +1,34 @@
 import { fetchResasApi } from './fetchResasApi';
 import { jobOpenings } from './globalVariable';
 
+type JobData = {
+  broadCode: number;
+  broadName: string;
+  middleCode: string;
+  middleName: string;
+  value: number;
+};
+
 /**
  * RESAS_APIから職業分類名と求人・求職者数を取得し表示させる関数
  */
 export function displayJobSeeker(index: number): void {
-  jobOpenings.innerHTML += `<div class="p-loader">Loading...</div>`;
+  jobOpenings ? (jobOpenings.innerHTML += `<div class="p-loader">Loading...</div>`) : undefined;
   if (index > 0) {
     fetchResasApi(
       `https://opendata.resas-portal.go.jp/api/v1/regionalEmploy/analysis/portfolio?prefCode=${index}&year=2012&matter=1&class=1`
-    ).then((data) => {
-      jobOpenings.innerHTML = '';
-      for (let i = 0; i < data.result.data.length; i++) {
-        jobOpenings.innerHTML += `
-        <dt class="p-job-openings__dt">${data.result.data[i].broadName}</dt>
-        <dd class="p-job-openings__dd">${data.result.data[i].value.toLocaleString()}人</dd>`;
+    ).then(data => {
+      const response: [JobData] = data.result.data;
+      jobOpenings ? (jobOpenings.innerHTML = '') : undefined;
+      for (let i = 0; i < response.length; i++) {
+        jobOpenings
+          ? (jobOpenings.innerHTML += `
+        <dt class="p-job-openings__dt">${response[i].broadName}</dt>
+        <dd class="p-job-openings__dd">${response[i].value.toLocaleString()}人</dd>`)
+          : undefined;
       }
     });
   } else {
-    jobOpenings.innerHTML = '';
+    jobOpenings ? (jobOpenings.innerHTML = '') : undefined;
   }
 }
